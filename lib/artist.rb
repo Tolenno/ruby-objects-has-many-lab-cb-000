bb25bc899151c180734646b70c259c1ae6440176
Matt Hough <matt@hough.tv>
2017-10-08 11:52:30 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -11,7 +11,7 @@ class Artist
 
   def add_song(song)
     # take in argument of song from Song:class then adds that song to @song array and tells the song that it belongs to that artist
-    @@songs << @songs << song
+    @@total_songs << @songs << song
     song.artist = self
   end
 
